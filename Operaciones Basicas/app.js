e7a3c548bde9a9928696e5c8928c93d917c4834c
IsraelHernandezFES <jesushernandezpacheco0705@gmail.com>
2026-02-27 17:29:30 -0600

    Agregada carpeta de proyecto Web 2

---
+++ Operaciones Basicas/app.js
@@ -0,0 +1,20 @@
+/**
+ * Hernandez Pacheco Jesus Israel
+ */
+
+// --- 1. OPERADORES ARITMÉTICOS ---
+let a = 10;
+let b = 3;
+
+console.log("Suma:", a + b);           // 13
+console.log("Resta:", a - b);          // 7
+console.log("Multiplicación:", a * b); // 30
+console.log("División:", a / b);       // 3.333...
+console.log("Módulo (Resto):", a % b); // 1
+console.log("Exponente:", a ** b);     // 1000 (10^3)
+
+// --- 2. OPERADORES DE COMPARACIÓN ---
+console.log("Igualdad (valor):", a == "10");     // true
+console.log("Igualdad Estricta:", a === "10");   // false (compara valor y tipo)
+console.log("Desigualdad:", a != b);             // true
+console.log("Mayor que:", a > b);                // true
