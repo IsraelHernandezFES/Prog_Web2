074526dc53db6ab3c5f1244377487c51f85b5552
IsraelHernandezFES <jesushernandezpacheco0705@gmail.com>
2026-02-28 16:28:51 -0600

    se agrega Operaciones Logicas

---
+++ Operaciones Logicas/app.js
@@ -0,0 +1,20 @@
+/**
+ * Hernandez Pacheco Jesus Israel
+ */
+
+// 1 OPERADORES LÓGICOS ---
+let esVerdadero = true;
+let esFalso = false;
+
+console.log("AND Lógico:", esVerdadero && esFalso); // false
+console.log("OR Lógico:", esVerdadero || esFalso);  // true
+console.log("NOT Lógico:", !esVerdadero);           // false
+
+// 2 OPERACIONES CON STRINGS ---
+let nombre = "Jesus";
+let apellido = "Hernandez";
+
+// Concatenación tradicional y Template Literals
+console.log("Concatenar:", nombre + " " + apellido);
+console.log(`Template Literal: Hola ${nombre}, bienvenido.`);
+
