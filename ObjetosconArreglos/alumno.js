d5f5e8ae947a2070bd6d3ca7c98904896e9cdaac
IsraelHernandezFES <jesushernandezpacheco0705@gmail.com>
2026-03-25 20:43:45 -0600

    se agrego Objetos usando arreglos

---
+++ ObjetosconArreglos/alumno.js
@@ -0,0 +1,37 @@
+export let alumno = {
+    id: 0,
+    nombre: "",
+    primerApellido: "",
+    segundoApellido: "",
+    edad: 0,
+    titulado: false,
+    egresado: {
+        estado: true
+    },
+    domicilio: { 
+        calle: {
+            nombre: "",
+            numero: "",
+            interior: "",
+            manzana: 0,
+            lote: 0
+        },
+        numero: "",
+        colonia: "",
+        CP: "",
+        alcaldia: "",
+        estado: "",
+        pais: "",
+        continente: ""
+    },
+    kinder: {
+        nombre: "",
+        direccion: ""
+
+    },
+    primaria: {
+        nombre: "",
+        direccion: ""
+    }
+}
+
